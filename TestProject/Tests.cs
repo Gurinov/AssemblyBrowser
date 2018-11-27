172ce48352e59d3558049decca0d7f27744ca8c2
Gurinov <evgeniy-gurinov@mail.ru>
2018-11-28 02:53:28 +0300

    Add tests

--- TestProject/Tests.cs
+++ TestProject/Tests.cs
@@ -10,14 +10,25 @@ namespace TestProject
     public class Tests
     {
         
-        private AssemblyBrowser _assemblyBrowser = new AssemblyBrowser(AppDomain.CurrentDomain.BaseDirectory + "TestProject.dll");
-        //private AssemblyBrowser _assemblyBrowser = new AssemblyBrowser(AppDomain.CurrentDomain.BaseDirectory + "../../Plugins/1.dll");
+        //private AssemblyBrowser _assemblyBrowser = new AssemblyBrowser(AppDomain.CurrentDomain.BaseDirectory + "TestProject.dll");
+        private AssemblyBrowser _assemblyBrowser;
 
         private const int namespaceCount = 2;
         private const int DataTypeCount = 4;
         private const int fieldCount = 6;
         private const int methodCount = 7;
-        private const int propertyCount = 6;
+        private const int propertyCount = 0;
+        
+        private const string namespaceName = "Tracer";
+        private const string DataTypeName = "public class Node";
+        private const string fieldName = "public String _className";
+        private const string methodName = "public Node getParent ()";
+
+        [SetUp]
+        public void Initialization()
+        {
+            _assemblyBrowser = new AssemblyBrowser(AppDomain.CurrentDomain.BaseDirectory + "../../Plugins/1.dll");
+        }
 
         [Test]
         public void NamespaceCountTest()
@@ -46,7 +57,31 @@ namespace TestProject
         [Test]
         public void PropertyCountTest()
         {
-            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetDataTypes()[0].GetFields().Count, propertyCount);
+            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetDataTypes()[0].GetProperties().Count, propertyCount);
+        }
+        
+        [Test]
+        public void NamespaceConsistTest()
+        {
+            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetName(), namespaceName);
+        }
+        
+        [Test]
+        public void DataTypeConsistTest()
+        {
+            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetDataTypes()[0].GetName(), DataTypeName);
+        }
+
+        [Test]
+        public void FieldConsistTest()
+        {
+            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetDataTypes()[0].GetFields()[0].GetName(), fieldName);
+        }
+
+        [Test]
+        public void MethodConsistTest()
+        {
+            Assert.AreEqual(_assemblyBrowser.GetAllNamespaces()[0].GetDataTypes()[0].GetMethods()[0].GetName(), methodName);
         }
     }
 }
